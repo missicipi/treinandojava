0874475dabee970e565d2ae5e513c5eced694123
missicipi <professorulissesm@gmail.com>
2020-06-16 09:53:00 -0300

    Finalizando triangle

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,10 +1,41 @@
 package Aplication;
 
+import java.util.Scanner;
+
+import Entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Triangle x, y;
 
+		Scanner sc = new Scanner(System.in);
+
+		x = new Triangle();
+		y = new Triangle();
+
+		System.out.println("Entre com os valores do tringulo x");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+
+
+		System.out.println("Entre com os valores do tringulo y");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+
+		x.area();
+		y.area();
+		System.out.println(x.area());
+		System.out.println(y.area());
+		
+		
+		if (x.area() > y.area()) {
+			System.out.println("X é maior");
+		}else {
+			System.out.println("Y é maior");}
+		sc.close();
 	}
 
 }
